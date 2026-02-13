4c047231f38fae5b95b8ee0abafbee6a851fbe08
phucpt <phuctanpham@gmail.com>
2026-02-13 05:29:26 +0000

    fix(website): ignore broken anchors

--- frontend/website/docusaurus.config.ts
+++ frontend/website/docusaurus.config.ts
@@ -22,10 +22,11 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Garblo',
+  projectName: 'Garblo',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
+  onBrokenAnchors: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
