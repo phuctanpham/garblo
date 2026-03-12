a8822233d90ef1398c8cf55b5502b0cb15a06ba3
nghibg <nghibui1112@gmail.com>
2026-03-12 17:46:33 +0000

    feat(website): add new sections to homepage
    by nghibg

--- frontend/website/src/pages/index.tsx
+++ frontend/website/src/pages/index.tsx
@@ -1,5 +1,6 @@
 /* frontend/website/src/pages/index.tsx */
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from 'react';
 import Layout from '@theme/Layout';
 
@@ -17,6 +18,15 @@ import HowItWorks from '@site/src/components/HomepageFeatures/section2'
 import Section1 from '@site/src/components/Home/section1' // Import khối Section 1 vào
 import Section2 from '@site/src/components/Home/section2'
 >>>>>>> 2fe9656 (refactor(website): change props of section2 component)
+=======
+import React from 'react';
+import Layout from '@theme/Layout';
+
+// Import đầy đủ cả 3 Section ở đây
+import Section1 from '@site/src/components/Home/section1';
+import Section2 from '@site/src/components/Home/section2';
+import Section3 from '@site/src/components/Home/section3';
+>>>>>>> e2378e7 (feat(website): add new sections to homepage)
 
 export default function Home(): JSX.Element {
   return (
@@ -27,12 +37,17 @@ export default function Home(): JSX.Element {
       <main>
         <Section1 />
 <<<<<<< HEAD
+<<<<<<< HEAD
         <Section3 />
 =======
 
         {/* Sau này làm Section 2, 3 sẽ thêm vào đây */}
         <Section2 />
 >>>>>>> 2fe9656 (refactor(website): change props of section2 component)
+=======
+        <Section2 />
+        <Section3 />
+>>>>>>> e2378e7 (feat(website): add new sections to homepage)
       </main>
       
     </Layout>
