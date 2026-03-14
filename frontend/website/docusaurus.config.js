89bb5d94d29761569f5c08a2c7bcf22ac81d6d1e
phucpt <phuctanpham@gmail.com>
2026-03-14 04:00:48 +0000

    Merge frontend/website from nghibg to main

--- frontend/website/docusaurus.config.js
+++ frontend/website/docusaurus.config.js
@@ -1,113 +1,115 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var prism_react_renderer_1 = require("prism-react-renderer");
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
+var prism_react_renderer_1 = require('prism-react-renderer')
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 var config = {
-    title: 'Garblo',
-    tagline: 'Reinvent Fashion',
-    favicon: 'img/favicon.ico',
-    // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-    future: {
-        v4: true, // Improve compatibility with the upcoming Docusaurus v4
-    },
-    // Set the production url of your site here
-    url: 'https://your-docusaurus-site.example.com',
-    // Set the /<baseUrl>/ pathname under which your site is served
-    // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'Garblo',
-    projectName: 'Garblo',
-    onBrokenLinks: 'warn',
-    onBrokenAnchors: 'warn',
-    // Even if you don't use internationalization, you can use this field to set
-    // useful metadata like html lang. For example, if your site is Chinese, you
-    // may want to replace "en" with "zh-Hans".
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en'],
-    },
-    presets: [
-        [
-            'classic',
-            {
-                docs: {
-                    sidebarPath: './sidebars.ts',
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-                },
-                blog: {
-                    showReadingTime: true,
-                    feedOptions: {
-                        type: ['rss', 'atom'],
-                        xslt: true,
-                    },
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-                    // Useful options to enforce blogging best practices
-                    onInlineTags: 'warn',
-                    onInlineAuthors: 'warn',
-                    onUntruncatedBlogPosts: 'warn',
-                },
-                theme: {
-                    customCss: './src/css/custom.css',
-                },
-            },
-        ],
+  title: 'Garblo',
+  tagline: 'Reinvent Fashion',
+  favicon: 'img/favicon.ico',
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Garblo',
+  projectName: 'Garblo',
+  onBrokenLinks: 'warn',
+  onBrokenAnchors: 'warn',
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      },
     ],
-    themeConfig: {
-        // Replace with your project's social card
-        image: 'img/docusaurus-social-card.jpg',
-        colorMode: {
-            respectPrefersColorScheme: true,
-        },
-        navbar: {
-            title: 'Garblo',
-            logo: {
-                alt: 'Garblo Logo',
-                src: 'img/logo.svg', // Giữ nguyên tên file logo hiện tại của bạn
-            },
-            items: [
-                // Đã xóa 'Tutorial'
-                { to: '/blog', label: 'Blog', position: 'left' },
-                {
-                    href: 'https://github.com/phuctanpham/garblo', // Link GitHub chuẩn
-                    label: 'GitHub',
-                    position: 'right',
-                },
-            ],
-        },
-        footer: {
-            style: 'dark',
-            links: [
-                {
-                    title: 'Garblo',
-                    items: [
-                        {
-                            label: 'Blog',
-                            to: '/blog',
-                        },
-                    ],
-                },
-                {
-                    title: 'More',
-                    items: [
-                        {
-                            label: 'GitHub',
-                            href: 'https://github.com/phuctanpham/garblo',
-                        },
-                    ],
-                },
-            ],
-            copyright: "Copyright \u00A9 2026 Garblo. Built with Docusaurus.",
-        },
-        prism: {
-            theme: prism_react_renderer_1.themes.github,
-            darkTheme: prism_react_renderer_1.themes.dracula,
-        },
+  ],
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      respectPrefersColorScheme: true,
     },
-};
-exports.default = config;
+    navbar: {
+      title: 'Garblo',
+      logo: {
+        alt: 'Garblo Logo',
+        src: 'img/logo.svg', // Giữ nguyên tên file logo hiện tại của bạn
+      },
+      items: [
+        // Đã xóa 'Tutorial'
+        { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/phuctanpham/garblo', // Link GitHub chuẩn
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Garblo',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/phuctanpham/garblo',
+            },
+          ],
+        },
+      ],
+      copyright: 'Copyright \u00A9 2026 Garblo. Built with Docusaurus.',
+    },
+    prism: {
+      theme: prism_react_renderer_1.themes.github,
+      darkTheme: prism_react_renderer_1.themes.dracula,
+    },
+  },
+}
+exports.default = config
