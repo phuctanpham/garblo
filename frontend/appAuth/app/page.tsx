cd7152c74a28eab74fca4cf18d8da003db47dd49
phucpt <phuctanpham@gmail.com>
2026-03-24 13:12:44 +0000

    refactor: phucpt update pnpm

--- frontend/appAuth/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Auth App</h1>
-    </main>
-  )
-}
--- frontend/appConsumer/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss'
-import sharedConfig from '@garblo/ui/tailwind.config'
-
-const config: Pick<Config, 'prefix' | 'presets'> = {
-  prefix: sharedConfig.prefix,
-  presets: [sharedConfig],
-}
-
-export default config
